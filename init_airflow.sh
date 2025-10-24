fefe46419044eb5bfb9478a0926534e35009a004
benjsant <santrisseb@gmail.com>
2025-10-24 08:31:42 +0200

    modification script initialisation

--- init_airflow.sh
+++ init_airflow.sh
@@ -7,11 +7,11 @@ set -e  # exit on first error
 # Install MailCatcher
 # -----------------------
 sudo apt update
-sudo apt install -y ruby ruby-dev build-essential libsqlite3-dev graphviz
+sudo apt install -y ruby ruby-dev build-essential libsqlite3-dev graphviz python3-venv
 sudo gem install mailcatcher
 
 # Run MailCatcher in background
-mailcatcher &
+mailcatcher
 echo "📬 MailCatcher running on http://localhost:1080"
 
 # -----------------------
@@ -29,6 +29,20 @@ pip install -r requirements.txt
 
 # Set AIRFLOW_HOME
 export AIRFLOW_HOME=$(pwd)/airflow
+
+# Initialize Airflow to generate config if it doesn't exist
+if [ ! -f "$AIRFLOW_HOME/airflow.cfg" ]; then
+    echo "Initializing Airflow to generate config..."
+    airflow db migrate
+fi
+
+# Option to use relative path for DB
+if [[ " $@ " =~ " --relative-db-path " ]]; then
+    echo "Updating airflow.cfg to use relative path for database..."
+    # Use a different delimiter for sed because of the slashes in the path
+    sed -i "s#^sql_alchemy_conn = .*#sql_alchemy_conn = sqlite:////$(pwd)/airflow/airflow.db#" "$AIRFLOW_HOME/airflow.cfg"
+    echo "✅ sql_alchemy_conn updated."
+fi
 
 # Import Airflow variables if file exists
 if [ -f "dags/data/variables.json" ]; then
